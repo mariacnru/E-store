320189e2e73c3dc3ac1599307d5df81deefe03d2
maria <mariacnru@gmail.com>
2026-07-07 10:28:27 +0330

    hover effect added to CategorieCard

--- src/Pages/Home/components/Categories/CategorieCard.jsx
+++ src/Pages/Home/components/Categories/CategorieCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function CategorieCard({ icon, title }) {
   return (
-    <div className="bg-gray-100 w-40 rounded-lg p-5 flex flex-col items-center gap-2">
+    <div className="bg-gray-100 w-40 rounded-lg p-5 flex flex-col items-center gap-2 hover:bg-violet-600 hover:text-white hover:-translate-y-4 cursor-pointer transition-all">
       <span>{icon}</span>
       <p className="text-sm">{title}</p>
     </div>
