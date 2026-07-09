7cec28ab7a5d4eeec4348b746e43708824e1657f
maria <mariacnru@gmail.com>
2026-07-09 10:26:34 +0330

    Home page productd section filter logic added

--- src/Pages/Home/components/Products/Products.jsx
+++ src/Pages/Home/components/Products/Products.jsx
@@ -1,17 +1,33 @@
-import React, { useState } from "react";
-import ProductsFilter from "./PruductsFilter";
+import React, { useMemo, useState } from "react";
 import homePageProducts from "../../../../Data/HomePageProducts";
 import ProductCard from "./ProductCard";
+import ProductsFilter from "./PruductsFilter";
 
 function Products() {
-  const [products, setProducts] = useState(homePageProducts);
   const [productsFilter, setProductsFilter] = useState("newArrival");
+
+  const products = useMemo(() => {
+    switch (productsFilter) {
+      case "bestSeller":
+        return [...homePageProducts].sort((a, b) => b.saleCount - a.saleCount);
+
+      case "featuredProducts":
+        return homePageProducts.filter((product) => product.discount);
+
+      case "newArrival":
+      default:
+        return [...homePageProducts].sort((a, b) => b.id - a.id);
+    }
+  }, [productsFilter]);
 
   return (
     <div className="px-30 py-10 bg-white">
-      <ProductsFilter setFilter={setProductsFilter} />
+      <ProductsFilter
+        productsFilter={productsFilter}
+        setFilter={setProductsFilter}
+      />
 
-      <div className="grid grid-cols-4 gap-2">
+      <div className="grid grid-cols-4 gap-6">
         {products.map((product) => (
           <ProductCard key={product.id} {...product} />
         ))}
--- src/Pages/Home/components/Products/PruductsFilter.jsx
+++ src/Pages/Home/components/Products/PruductsFilter.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 
-function ProductsFilter({ setFilter }) {
+function ProductsFilter({ productsFilter, setFilter }) {
   return (
     <ul className="text-sm flex gap-5 mb-5">
-      <li onClick={() => setFilter("newArrival")} className="cursor-pointer">
+      <li
+        onClick={() => setFilter("newArrival")}
+        className={`${
+          productsFilter === "newArrival"
+            ? "font-danaMedium underline underline-offset-8"
+            : ""
+        } cursor-pointer transition-all`}
+      >
         جدیدترین محصولات
       </li>
-      <li onClick={() => setFilter("bestSeller")} className="cursor-pointer">
+
+      <li
+        onClick={() => setFilter("bestSeller")}
+        className={`${
+          productsFilter === "bestSeller"
+            ? "font-danaMedium underline underline-offset-8"
+            : ""
+        } cursor-pointer transition-all`}
+      >
         پرفروش‌ترین محصولات
       </li>
-      <li onClick={() => setFilter("featuredProducts")} className="cursor-pointer">
+
+      <li
+        onClick={() => setFilter("featuredProducts")}
+        className={`${
+          productsFilter === "featuredProducts"
+            ? "font-danaMedium underline underline-offset-8"
+            : ""
+        } cursor-pointer transition-all`}
+      >
         محصولات ویژه
       </li>
     </ul>
