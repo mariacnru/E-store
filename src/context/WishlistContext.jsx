b70e54c16062b0b100a1914e967c22714878f71c
maria <mariacnru@gmail.com>
2026-07-10 09:29:12 +0330

    WishlistContext modified

--- src/context/WishlistContext.jsx
+++ src/context/WishlistContext.jsx
@@ -1,32 +1,21 @@
-// context/WishlistContext.jsx
-
 import { createContext, useState } from "react";
 
 export const WishlistContext = createContext();
 
-function WishlistProvider({ children }) {
+export default function WishlistProvider({ children }) {
   const [wishlist, setWishlist] = useState([]);
 
   const toggleWishlist = (product) => {
-    const isExist = wishlist.some((item) => item.id === product.id);
-
-    if (isExist) {
-      setWishlist(wishlist.filter((item) => item.id !== product.id));
-    } else {
-      setWishlist([...wishlist, product]);
-    }
+    setWishlist((prev) =>
+      prev.some((item) => item.id === product.id)
+        ? prev.filter((item) => item.id !== product.id)
+        : [...prev, product],
+    );
   };
 
   return (
-    <WishlistContext.Provider
-      value={{
-        wishlist,
-        toggleWishlist,
-      }}
-    >
+    <WishlistContext.Provider value={{ wishlist, toggleWishlist }}>
       {children}
     </WishlistContext.Provider>
   );
 }
-
-export default WishlistProvider;
